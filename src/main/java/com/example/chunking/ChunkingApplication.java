115ffd465ce57ffea936f70c1bd52dfe7a5813db
James Collerton <james.collerton@bbc.co.uk>
2021-09-24 08:58:06 +0100

    Adding rest of skeletong project

---
+++ src/main/java/com/example/chunking/ChunkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.chunking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chunking/ChunkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chunking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChunkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
